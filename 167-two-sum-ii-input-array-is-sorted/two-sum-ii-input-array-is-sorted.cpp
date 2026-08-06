d046614d52831505ff18feeffcbf77c094cd1567
Prabhav Gomase <prabhavtg@gmail.com>
2026-08-06 21:42:55 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (95.77%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-      unordered_map<int,int>mp; 
-      for(int i=0;i<numbers.size();i++)
-      {
-        int diff=target-numbers[i];
-        if(mp.find(diff)!=mp.end())
+    //   unordered_map<int,int>mp; 
+    //   for(int i=0;i<numbers.size();i++)
+    //   {
+    //     int diff=target-numbers[i];
+    //     if(mp.find(diff)!=mp.end())
+    //     {
+    //         return {mp[diff]+1,i+1};
+    //     }
+    //     mp[numbers[i]]=i;
+    //   }
+    //   return {-1,-1};
+    int low=0,high=numbers.size()-1;
+    while(low<high)
+    {
+        int sum=numbers[low]+numbers[high];
+        if(sum==target)
+        return {low+1,high+1};
+        else if(sum>target)
         {
-            return {mp[diff]+1,i+1};
+            high--;
         }
-        mp[numbers[i]]=i;
-      }
-      return {-1,-1};
+        else
+        {
+            low++;
+        }
+    }
+    return {0,0};
     }
 };
