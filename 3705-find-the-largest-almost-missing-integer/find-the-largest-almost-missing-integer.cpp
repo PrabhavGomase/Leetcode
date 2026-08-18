ff9a7229db60b52f7850f69402ceba14c11b075b
Prabhav Gomase <prabhavtg@gmail.com>
2026-08-18 22:46:26 +0530

    Time: 3 ms (54.14%) | Memory: 29.4 MB (56.02%) - LeetSync

--- 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.cpp
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.cpp
@@ -4,6 +4,11 @@ public:
         int maximum=0;
         int a1=nums[0];int a2=nums[nums.size()-1];
         unordered_map<int,int>mp;
+        int ans=-1;
+        for(int i= 0;i<nums.size();i++)
+        {
+            mp[nums[i]]++;
+        }
         if(k==nums.size())
         {
         for(int i=0;i<nums.size();i++)
@@ -12,12 +17,7 @@ public:
         }
         return maximum;
         }
-        for(int i= 0;i<nums.size();i++)
-        {
-            mp[nums[i]]++;
-        }
-        int ans=-1;
-        if(k==1)
+        else if(k==1)
         {
             for(int i= 0;i<nums.size();i++)
         {
@@ -29,6 +29,8 @@ public:
         }
         return ans;
         }   
+        else 
+        {
         int count1=mp[nums[0]];int count2=mp[nums[nums.size()-1]];
         if(count1==1 && count2>1)
         return a1;
@@ -37,6 +39,7 @@ public:
         else if(count1==1 && count2==1)
         {
             return max(a1,a2);
+        }
         }
     return -1;
     }
