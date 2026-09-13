6d73b2426d86a175e6be2d84e7f10039e9eec713
Prabhav Gomase <prabhavtg@gmail.com>
2026-09-14 02:15:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (67.25%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if(s.size()%2!=0)
+        return false;
+        stack<char>st;
+        for (char ch : s) {
+          if(ch=='(' || ch=='{'|| ch=='[')
+          st.push(ch);
+          else
+          {
+            if(st.empty())  return false;
+            else if((ch==')' && st.top()!='(') || (ch=='}' && st.top()!='{') || (ch==']' && st.top()!='['))
+            return false;
+            st.pop();
+          }
+        }
+        return st.empty();
+    }
+};
