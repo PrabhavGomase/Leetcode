64b34120f2b07e097dc874394b72d2a14a8cd893
Prabhav Gomase <prabhavtg@gmail.com>
2026-09-20 21:30:25 +0530

    Time: 107 ms (75.20%) | Memory: 66.6 MB (40.77%) - LeetSync

--- 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.cpp
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.cpp
@@ -2,25 +2,27 @@ class Solution {
 public:
     int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
         unordered_map<int,int>mp;
+        vector<int>store;
         for(int i=0;i<arr.size();i++)
         {
            mp[arr[i]]++;
         }
-        vector<pair<int,int>> v(mp.begin(), mp.end());
-sort(v.begin(), v.end(), [](auto &a, auto &b) {
-    return a.second < b.second;
-});
-    int ans=0;
-        for(int i=0;i<v.size();i++)
+        for(auto it:mp)
+        {
+            store.push_back(it.second);
+        }
+        sort(store.begin(),store.end());
+         int ans=0;
+        for(int i=0;i<store.size();i++)
         {
                     
-            if(k<=0 && i<v.size())
+            if(k<=0 )
             { 
                 ans++;
             }
             else
             {
-                k=k-v[i].second;
+                k=k-store[i];
                 if(k<0)
                 ans=1;
                 else
@@ -29,6 +31,29 @@ sort(v.begin(), v.end(), [](auto &a, auto &b) {
 
         }
         return ans;
+//         vector<pair<int,int>> v(mp.begin(), mp.end());
+// sort(v.begin(), v.end(), [](auto &a, auto &b) {
+//     return a.second < b.second;
+// });
+//     int ans=0;
+//         for(int i=0;i<v.size();i++)
+//         {
+                    
+//             if(k<=0 && i<v.size())
+//             { 
+//                 ans++;
+//             }
+//             else
+//             {
+//                 k=k-v[i].second;
+//                 if(k<0)
+//                 ans=1;
+//                 else
+//                 ans=0;
+//             }
+
+//         }
+//         return ans;
         
         
         
